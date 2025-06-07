64833c890aed9085c5bfe734d3c53171863dc30e
Adrian Quiros <157768117+adrianqe@users.noreply.github.com>
2025-06-06 18:22:57 -0600

    Remove unused Categories and Tags components from main entry file

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './tailwind.css';
 import App from './App.jsx'
-import CategoriesComponent from './CategoriesComponent.jsx';
-import TagsComponent from './TagsComponent.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
